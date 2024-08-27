38a230adca44597c8b62baf92564c86c5f0f4d4a
Vinita Santosh Sonwane <vinitasonwane24@gmail.com>
2024-08-27 15:38:07 +0530

    Delete Banking-App directory

--- Banking-App/src/main/java/com/tka/repository/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tka.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tka.entity.Account;
-
-public interface AccountRepository extends JpaRepository<Account,Long> {
-
-}
